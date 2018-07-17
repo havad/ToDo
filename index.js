ae601ce406008f5d843da0e927de6248564d8b2d
havad <yeahprobably@gmail.com>
2018-07-17 12:43:05 -0500

    tasks can now be added and removed easily

--- index.js
+++ index.js
@@ -1,3 +1,50 @@
-function button() {
-    console.log(document.getElementById("text").value);
+var count = 0;      //this is used to label the divs and buttons created
+
+//Function should read the text from the box and create a paragraph and x button that acts as a task on the webpage
+//if no value is in the text box nothing happens
+function submitButton() {
+
+    //check to make sure the text box has a value
+    if (document.getElementById("text").value == ""){
+        console.log("nothing submitted");
+        return;
+    }
+
+    //increase count and we need to create a div and paragraph to store the task
+    count++;
+    console.log("count is " + count);
+    var newDiv = document.createElement("div");
+    var para = document.createElement("p");
+    var text = document.createTextNode(document.getElementById("text").value);
+    para.appendChild(text);
+
+    console.log(text);
+
+    //we need a button to click to remove an element which is done here
+    var xButton = document.createElement("button");
+    xButton.setAttribute("id", "xButton" + count);
+    xButton.setAttribute("onclick", "removeTask(this.parentElement.id)");
+    var x = document.createTextNode("x");
+    xButton.appendChild(x);
+
+    para.style.display = "inline-block";
+    
+    //finish by appending everything to the newDiv
+    newDiv.setAttribute("id", "task" + count);
+    newDiv.appendChild(para);
+    newDiv.appendChild(xButton);
+    document.getElementById("tasks").appendChild(newDiv);
+
+    document.getElementById("text").value = "";
+}
+
+//function should remove task and x button so that tasks may be cleared out when finished
+function removeTask(id) {
+
+    var test = document.getElementById(id)
+    test.parentNode.removeChild(test);
+
+    console.log("removing " + id + "!");
+    count--;
+    console.log("count is " + count);
 }
